988dce560caccd3e74701e4dbee18589944f0450
TheGBO <gabrielthejota@gmail.com>
2022-03-04 16:18:59 -0300

    trying to fix react deploy again

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use('/api', routes);
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static("client","build"));
+app.use(express.static("client/build"));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,"client","build","index.html"))
 })
