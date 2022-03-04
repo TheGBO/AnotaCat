339e2eb72bbad8d5e96d9117511d215e6fb300de
TheGBO <gabrielthejota@gmail.com>
2022-03-04 16:16:35 -0300

    trying to fix react build again

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const routes = require('./api/routes');
+const path = require('path');
 
 require('dotenv').config({path:"./config/.env"});
 app.use(cors());
@@ -10,8 +11,9 @@ app.use('/api', routes);
 
 const port = process.env.PORT || 5000;
 
+app.use(express.static("client","build"));
 app.get('/', (req, res) => {
-    res.json({success:true});
+    res.sendFile(path.join(__dirname,"client","build","index.html"))
 })
 
 app.listen(port, () => {
